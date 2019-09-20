eed3dc2020f32dbab0c4affd95a2fe3c6ef4597c
Aparna <aparnarajugottumukkala@gmail.com>
2019-09-21 00:15:50 +0800

    this is booking microservice

---
+++ src/main/java/com/nus/iss/eatngreet/booking/repository/ItemRepository.java
@@ -0,0 +1,20 @@
+package com.nus.iss.eatngreet.booking.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.nus.iss.eatngreet.booking.entity.ItemEntity;
+
+@Repository
+public interface ItemRepository extends JpaRepository<ItemEntity, Long> {
+
+	@Query("Select i from ItemEntity i where i.name like %:name% and isDeleted=false and isActive=true")
+	Optional<List<ItemEntity>> itemNameAutoSuggest(@Param("name") String itemName, Pageable pageable);
+
+}
---
+++ src/main/java/com/nus/iss/eatngreet/booking/repository/ProducerOrderRepository.java
@@ -0,0 +1,18 @@
+package com.nus.iss.eatngreet.booking.repository;
+
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nus.iss.eatngreet.booking.entity.ProducerOrderEntity;
+
+@Repository
+public interface ProducerOrderRepository extends CrudRepository<ProducerOrderEntity, Long> {
+
+	@Query("select p from ProducerOrderEntity p where p.servingDate >= CURRENT_DATE and p.reservationDeadline >= CURRENT_DATE and p.isActive = true and p.isDeleted = false")
+	List<ProducerOrderEntity> findAllProducerRecords(Pageable pageable);
+
+}
